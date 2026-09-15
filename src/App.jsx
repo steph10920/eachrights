a555d35e365ef04d4f92f955f424bbb00e8068ae
Stephen Ashioya <sashioyas@gmail.com>
2026-09-15 16:05:14 +0300

    innitial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+
 import Home from "./pages/Home";
 import About from "./pages/About";
 import OurWork from "./pages/OurWork";
@@ -10,6 +12,8 @@ import Gallery from "./pages/Gallery";
 import Opportunities from "./pages/Opportunities";
 import Donors from "./pages/Donors";
 import Contact from "./pages/Contact";
+
+// Programmes
 import EducationJustice from "./pages/programmes/EducationJustice";
 import GenderJustice from "./pages/programmes/GenderJustice";
 import HealthJustice from "./pages/programmes/HealthJustice";
@@ -24,38 +28,46 @@ function App() {
       <Navbar />
 
       <Routes>
+        {/* Main Pages */}
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/our-work" element={<OurWork />} />
         <Route path="/resources" element={<Resources />} />
         <Route path="/gallery" element={<Gallery />} />
         <Route path="/opportunities" element={<Opportunities />} />
         <Route path="/donors" element={<Donors />} />
         <Route path="/contact" element={<Contact />} />
+
+        {/* Our Work / Programmes */}
         <Route
           path="/our-work/programmes/education-justice"
           element={<EducationJustice />}
         />
+
         <Route
           path="/our-work/programmes/gender-justice"
           element={<GenderJustice />}
         />
+
         <Route
           path="/our-work/programmes/health-justice"
           element={<HealthJustice />}
         />
+
         <Route
           path="/our-work/programmes/environmental-climate-justice"
           element={<EnvironmentalClimateJustice />}
         />
+
         <Route
           path="/our-work/programmes/economic-justice"
           element={<EconomicJustice />}
         />
+
         <Route
           path="/our-work/programmes/business-human-rights"
           element={<BusinessHumanRights />}
         />
+
         <Route
           path="/our-work/programmes/theory-of-change"
           element={<TheoryOfChange />}
