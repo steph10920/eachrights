269475ebbc0ad050f62360c602075bff0144dc89
Stephen Ashioya <sashioyas@gmail.com>
2026-09-18 12:46:14 +0300

    videos links added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -28,6 +29,7 @@ import UniversalPeriodicReview from "./pages/processes/UniversalPeriodicReview";
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Navbar />
 
       <Routes>
